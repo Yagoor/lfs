008342471ba3af0739f9c66807deb1800412620f
Yago Rosario <yago.rosario@hotmail.com.br>
2018-01-10 19:45:11 +0000

    Improved vimrc

--- extra/10.22-enviroment.sh
+++ extra/10.22-enviroment.sh
@@ -220,24 +220,26 @@ set hlsearch
 set ignorecase
 set smartcase
 set backspace=indent,eol,start
-set autoindent
 set nostartofline
 set ruler
-set laststatus=2
 set confirm
 set visualbell
 set t_vb=
-set mouse=a
-set cmdheight=2
+set mouse-=a
 set number
 set notimeout ttimeout ttimeoutlen=200
 set pastetoggle=<F11>
-set shiftwidth=4
-set softtabstop=4
-set expandtab
 
 map Y y$
 nnoremap <C-L> :nohl<CR><C-L>
+
+au TabLeave * let g:lasttab = tabpagenr()
+nnoremap <silent> <c-l> :exe "tabn ".g:lasttab<cr>
+vnoremap <silent> <c-l> :exe "tabn ".g:lasttab<cr>
+set nu
+set foldmethod=indent
+set foldlevel=0
+set foldclose=all
 
 " End /etc/vimrc
 EOF
